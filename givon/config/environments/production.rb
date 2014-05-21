27f126a943e0c7ec7a9cc8b8074010da28b7d2ef
Jason van der Merwe <jason.merwe@gmail.com>
2014-05-21 11:22:53 -0700

    added users

--- givon/config/environments/production.rb
+++ givon/config/environments/production.rb
@@ -4,6 +4,9 @@ Givon::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { :host => 'www.wegiveon.org' }
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
