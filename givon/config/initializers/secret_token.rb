faebac59a09c04f6381e762aae0d245cf7d1a1b7
Jason van der Merwe <jason.merwe@gmail.com>
2014-05-21 11:07:32 -0700

    made rails app

---
+++ givon/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Givon::Application.config.secret_key_base = 'a7a2615294fd35b62ff86a78254622fcb0c812a8bb5114c6fb6b578d1db48ca9f96315a36c2e80f82fc737b98e05a68883e8e03c2b7953eaf5a58eea14c79a5f'
---
+++ givon/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Givon::Application.config.session_store :cookie_store, key: '_givon_session'
